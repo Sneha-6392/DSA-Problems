0d16a0e578795394c1ae9129187a236ba3056788
Sneha Maurya <sneha.mauryasm02@gmail.com>
2025-10-13 22:00:00 +0530

    Time: 9 ms (22.85%), Space: 42.8 MB (20.88%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger n = new BigInteger(num1);
+        BigInteger m = new BigInteger(num2);
+         
+        BigInteger prod = n.multiply(m);
+        return String.valueOf(prod);
+
+    }
+}
