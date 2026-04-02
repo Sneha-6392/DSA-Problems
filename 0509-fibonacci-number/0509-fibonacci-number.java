f52ef97b070d2f6d14dfe6a35b2ddd8d2c439e6d
Sneha Maurya <sneha.mauryasm02@gmail.com>
2026-04-02 10:42:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) return n; 
-        return fib(n - 2) + fib(n - 1);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return recur(n, dp);
+    }
+    public int recur(int n, int[] dp) {
+        if (n <= 1) {
+            dp[n] = n;
+            return dp[n];
+        }
+
+        if (dp[n] != -1) return dp[n];
+
+        dp[n] = fib(n - 2) + fib(n - 1);
+        return dp[n];
     }
 }
