cd16f8e60140e680b4eae01ba5876c39eb6863cf
Sneha Maurya <sneha.mauryasm02@gmail.com>
2026-06-29 12:17:30 +0530

    Time: 19 ms (11.17%), Space: 46.2 MB (32.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println(s);
         int left = 0;
         int right = s.length() - 1;
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
-            left++;
-            right--;
+            else {
+                left++;
+                right--;
+            }
         }
+        
         return true;
     }
 }
