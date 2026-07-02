2fe2e6e3a449dcaa3983dcf1ccce25999b1d0b0f
Sneha Maurya <sneha.mauryasm02@gmail.com>
2026-07-02 12:51:22 +0530

    Time: 252 ms (9.57%), Space: 78.6 MB (90.69%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,34 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        int n = nums.length;
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        int range = max - min + 1;
+        int[] cnt = new int[range];
+
+        for (int num : nums) {
+            cnt[num - min]++;
+        }
+
+        for (int i = 1; i < range; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        int[] ans = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int val = nums[i];
+            ans[cnt[val - min] - 1] = val;
+            cnt[val - min]--;
+        }
+
+        return ans;
     }
 }
