0eb9ee9275271763f1b1bf658858130c11772676
Sneha Maurya <sneha.mauryasm02@gmail.com>
2026-07-21 09:42:58 +0530

    Time: 0 ms (100%), Space: 42.4 MB (55.99%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,14 +1,6 @@
 class Solution {
     public int trailingZeroes(int n) {
-        if (n == 0)
-            return 0;
-
-        int ans = 0;
-        while (n != 0) {
-            int result = n / 5;
-            n = result;
-            ans += result;
-        }
-        return ans;
+        if (n < 5) return 0;
+        return (n / 5) + trailingZeroes(n / 5);
     }
 }
